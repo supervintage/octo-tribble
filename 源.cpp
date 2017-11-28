e65949588b2c2bed45fb3bca7b540960f70e3e3b
supervintage <33148550+supervintage@users.noreply.github.com>
2017-11-27 19:45:57 -0600

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main() {
+	cout << "请输入两个数字" << endl;
+	double a, b;
+	cin >> a >> b;
+	double c = a + b;
+	cout << "结果为:" << c << endl;
+	system("pause");
+}
